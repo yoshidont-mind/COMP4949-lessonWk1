70663ec0d3156fb355d9588196ec8885cd19a5a8
yoshidont-mind <tyoshida6@my.bcit.ca>
2025-01-07 12:05:44 -0800

    Complete up to example 9

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import homePageView, aboutPageView, tatsuyaPageView
+from .views import homePageView, aboutPageView, tatsuyaPageView, homePost, results
 
 urlpatterns = [
     path('', homePageView, name='home'),
     path('about/', aboutPageView, name='about'),
     path('tatsuya/', tatsuyaPageView, name='tatsuya'),
+    path('homePost/', homePost, name='homePost'),
+    path('results/<int:choice>/<str:gmat>/', results, name='results'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import TemplateView
 
@@ -20,3 +20,35 @@ def homePageView(request):
         'firstName': 'Tatsuya',
         'lastName': 'Yoshida',
     })
+
+def homePost(request):
+    # Use request object to extract choice.
+
+    choice = -999
+    gmat = -999  # Initialize gmat variable.
+
+    try:
+        # Extract value from request object by control name.
+        currentChoice = request.POST['choice']
+        gmatStr = request.POST['gmat']
+
+        # Crude debugging effort.
+        print("*** Years work experience: " + str(currentChoice))
+        choice = int(currentChoice)
+        gmat = float(gmatStr)
+    # Enters 'except' block if integer cannot be created.
+    except:
+        return render(request, 'home.html', {
+            'errorMessage': '*** The data submitted is invalid. Please try again.',
+            'mynumbers': [1, 2, 3, 4, 5, 6, ]})
+    else:
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('results', kwargs={'choice': choice, 'gmat': gmat}, ))
+
+
+def results(request, choice, gmat):
+    print("*** Inside reults()")
+    return render(request, 'results.html', {'choice': choice, 'gmat': gmat})
+
